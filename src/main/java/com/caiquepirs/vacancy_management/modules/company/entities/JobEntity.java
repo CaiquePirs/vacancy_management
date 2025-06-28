554acf7a3d03bb1a186492161b49dfccc041db61
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-28 18:11:47 +0100

    Feat(Company): Create the job entity and relate it to the company table

---
+++ src/main/java/com/caiquepirs/vacancy_management/modules/company/entities/JobEntity.java
@@ -0,0 +1,44 @@
+package com.caiquepirs.vacancy_management.modules.company.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "job")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message = "This field is required")
+    private String description;
+    private String level;
+    private String benefits;
+
+    @NotBlank(message = "This field is required")
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private CompanyEntity companyId;
+
+    private UUID idCompany;
+
+    @CreationTimestamp
+    private LocalDateTime creationTimestamp;
+
+    @UpdateTimestamp
+    private LocalDateTime updateTimestamp;
+
+}
