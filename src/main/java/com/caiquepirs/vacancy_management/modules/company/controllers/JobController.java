687abd7c9a64f730bfcbc19a9007d94d254c8ed6
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-28 18:23:13 +0100

    Feat(Job): Create the JobController responsible for processing job-related requests
    - I created the first endpoint that creates a job vacancy by relating it to a company

---
+++ src/main/java/com/caiquepirs/vacancy_management/modules/company/controllers/JobController.java
@@ -0,0 +1,37 @@
+package com.caiquepirs.vacancy_management.modules.company.controllers;
+
+import com.caiquepirs.vacancy_management.modules.company.dto.CreateJobDTO;
+import com.caiquepirs.vacancy_management.modules.company.entities.JobEntity;
+import com.caiquepirs.vacancy_management.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/company/job")
+@AllArgsConstructor
+public class JobController {
+
+    private final CreateJobUseCase jobUseCase;
+
+    @PostMapping
+    public JobEntity create(@RequestBody @Valid CreateJobDTO jobDTO, HttpServletRequest request){
+        var companyId = request.getAttribute("company_id");
+
+       var jobEntity = JobEntity.builder()
+                .idCompany(UUID.fromString(companyId.toString()))
+                .benefits(jobDTO.benefits())
+                .description(jobDTO.description())
+                .level(jobDTO.level())
+                .build();
+
+        return jobUseCase.execute(jobEntity);
+    }
+
+}
