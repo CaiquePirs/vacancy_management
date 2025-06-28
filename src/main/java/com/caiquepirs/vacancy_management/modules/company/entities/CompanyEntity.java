a2fced2471240954c9f9bd67e5e77713bb8bec8e
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-28 18:10:52 +0100

    Feat(Company): Create the company entity

---
+++ src/main/java/com/caiquepirs/vacancy_management/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,47 @@
+package com.caiquepirs.vacancy_management.modules.company.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Pattern(regexp = "^\\S+$", message = "The field (username) cannot contain space.")
+    private String username;
+
+    @Email(message = "The field (email) must contain a valid email address.")
+    private String email;
+
+    @Length(min = 10, max = 50, message = "Password must contain between 10 and 50 characters")
+    private String password;
+
+    private String website;
+    private String name;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime creationTimestamp;
+
+    @UpdateTimestamp
+    private LocalDateTime updateTimestamp;
+
+    public CompanyEntity(){}
+
+
+}
