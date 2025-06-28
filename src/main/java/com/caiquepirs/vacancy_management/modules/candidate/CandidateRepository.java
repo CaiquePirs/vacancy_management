25b46273b674404cdb81d57b38db4f74f1eef3bd
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-28 18:09:42 +0100

    Feat(Candidate): Create the candidate repository

---
+++ src/main/java/com/caiquepirs/vacancy_management/modules/candidate/CandidateRepository.java
@@ -0,0 +1,11 @@
+package com.caiquepirs.vacancy_management.modules.candidate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+    Optional<CandidateEntity> findByNameOrEmail(String username, String email);
+    Optional<CandidateEntity> findByUsername(String username);
+}
