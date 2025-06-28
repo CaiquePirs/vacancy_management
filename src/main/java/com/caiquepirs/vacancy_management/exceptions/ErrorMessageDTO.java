1d292486f2d3b71ec162c16ea9d98268d6f27998
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-28 16:40:17 +0100

    Feat(Exceptions): Create the module to handle system exceptions
    - ErrorMessageDTO: To receive the error message and the field
    
    - GlobalExceptionHandler: to handle global errors
    
    - UserFoundException:If a user is found in the system with the same login

---
+++ src/main/java/com/caiquepirs/vacancy_management/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,4 @@
+package com.caiquepirs.vacancy_management.exceptions;
+
+public record ErrorMessageDTO(String message, String field) {
+}
---
+++ src/main/java/com/caiquepirs/vacancy_management/exceptions/UserFoundException.java
@@ -0,0 +1,7 @@
+package com.caiquepirs.vacancy_management.exceptions;
+
+public class UserFoundException extends RuntimeException {
+    public UserFoundException(String message) {
+        super(message);
+    }
+}
