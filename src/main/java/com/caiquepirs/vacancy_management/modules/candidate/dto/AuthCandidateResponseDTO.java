1c0b234854e3c4e7c694236a56da5a9ff0fc5d30
Caique Pires <c.pires@ba.estudante.senai.br>
2025-08-08 23:02:47 +0100

    refactor

--- src/main/java/com/caiquepirs/vacancy_management/modules/candidate/dto/AuthCandidateResponseDTO.java
+++ src/main/java/com/caiquepirs/vacancy_management/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Schema(description = "Data Transfer Object representing a candidate login")
 public class AuthCandidateResponseDTO {
 
-    @Schema(description = "Access token JWT", example = "eyJhbGciOiJIUzI1NiInR5cCI6IkpXVCJ9.eyJpc3MiOiJqb2JBUEkiLCJleHAiOjE3NTE1NjE5MTYsInN1YiI6IjdlNDJiZTE4LTIyMzYtNGFlMS1iY2ZkLTU1YjYyMDgxNTYyYSIsInJvbGVzIjpbIkNPTVBBTlkiXX0.ARhuXYJojDEHGC9yzngPQew0OEDJ6R72Fi-uR6bGmbY")
+    @Schema(description = "Access token JWT", example = "bearer<token>")
     private String access_token;
 
     @Schema(description = "Token expiration time", example = "1751561916465")
--- src/main/java/com/caiquepirs/vacancy_management/modules/company/dto/AuthCompanyResponseDTO.java
+++ src/main/java/com/caiquepirs/vacancy_management/modules/company/dto/AuthCompanyResponseDTO.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Schema(description = "Data Transfer Object representing a company login")
 public class AuthCompanyResponseDTO {
 
-    @Schema(description = "Access token JWT", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJqb2JBUEkiLCJleHAiOjE3NTE1NjE5MTYsInN1YiI6IjdlNDJiZTE4LTIyMzYtNGFlMS1iY2ZkLTU1YjYyMDgxNTYyYSIsInJvbGVzIjpbIkNPTVBBTlkiXX0.ARhuXYJojDEHGC9yzngPQew0OEDJ6R72Fi-uR6bGmbY")
+    @Schema(description = "Access token JWT", example = "bearer<token>")
     private String access_token;
 
     @Schema(description = "Token expiration time", example = "1751561916465")
